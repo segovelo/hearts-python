65287bd93e1855665c9cbb77e8250ebed5cf7f33
segovelo <gauchosinpingo@gmail.com>
2022-02-14 00:20:16 +0000

    Refactoring

--- hearts.py
+++ hearts.py
@@ -8,11 +8,10 @@ Created on Sun Feb 13 20:16:02 2022
 def top_of_heart(x):
   counter = 1
   midpoint = x//2+1
-  print("\n\n")
   for i in range(x):
-    print(" "*(midpoint-2)+"\u2764\uFE0F"*2*counter + " "*(midpoint - (counter-1)) + "\u2764\uFE0F"*2*counter)
+    print("\t" + " "*(midpoint-2)+"\u2764\uFE0F"*2*counter + " "*(midpoint - (counter-1)) + "\u2764\uFE0F"*2*counter)
     midpoint -= 2
-    if midpoint <= 3:
+    if midpoint <=3:
       break
     counter += 1
 
@@ -21,22 +20,24 @@ def full_heart(num_rows):
   num_columns = num_rows
   for i in range(num_rows):
     if num_rows == 2:
-      print(" "*(num_columns-1) + "\u2764\uFE0F ")
+      print("\t" + " "*(num_columns-1) + "\u2764\uFE0F ")
       break
     if counter == 0:
       top_of_heart(num_rows)
     elif counter == 1:
-      print(" "*i + "\u2764\uFE0F"*(num_rows-i))
+      print("\t" + " "*i + "\u2764\uFE0F"*(num_rows-i))
     elif counter == 2 or counter == 3:
-      print("\u2764\uFE0F"*num_rows)
+      print("\t" + "\u2764\uFE0F"*num_rows)
     else:
-      print(" "*(num_columns-(num_rows-2)) + "\u2764\uFE0F"*(num_rows-2))
+      print("\t" + " "*(num_columns-(num_rows-2)) + "\u2764\uFE0F"*(num_rows-2))
       num_rows = num_rows-2
       if num_rows == 1:
         break
     counter += 1
+ 
 
 
+print("\n\n  I  ")
 full_heart(20)
-
+print("  Shazz !!! ")
   
